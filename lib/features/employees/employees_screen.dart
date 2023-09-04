1e540dfbae0ac61c01524034d2448c81b371017d
Brayan Vargas <brayanvargasbarajas@gmail.com>
2023-09-04 16:39:59 -0500

    Change architecture to features,views,side menu navigation

---
+++ lib/features/employees/employees_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmployeesScreen extends StatelessWidget {
+  const EmployeesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Employees'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
---
+++ lib/features/roles/roles_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RolesScreen extends StatelessWidget {
+  const RolesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Roles'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
