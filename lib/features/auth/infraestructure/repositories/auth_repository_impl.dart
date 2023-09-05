845398f0a1bedcfd80d690343dcb7f995ece94c5
Brayan Vargas <brayanvargasbarajas@gmail.com>
2023-09-05 15:56:03 -0500

    Basic config auth_provider

--- lib/features/auth/infraestructure/repositories/auth_repository_impl.dart
+++ lib/features/auth/infraestructure/repositories/auth_repository_impl.dart
@@ -3,23 +3,23 @@ import 'package:cars_app/features/auth/domain/domain.dart';
 import 'package:cars_app/features/users/users.dart';
 
 class AuthRepositoryImpl extends AuthRepository {
-  final AuthDataSource dataSource;
+  final AuthDataSource _dataSource;
 
   AuthRepositoryImpl({AuthDataSource? dataSource})
-      : dataSource = dataSource ?? AuthDataSourceImpl();
+      : _dataSource = dataSource ?? AuthDataSourceImpl();
 
   @override
   Future<User> chechAuthStatus(String token) {
-    return dataSource.chechAuthStatus(token);
+    return _dataSource.chechAuthStatus(token);
   }
 
   @override
   Future<User> login(String email, String password) {
-    return dataSource.login(email, password);
+    return _dataSource.login(email, password);
   }
 
   @override
   Future<User> register(String email, String password, String fullName) {
-    return dataSource.register(email, password, fullName);
+    return _dataSource.register(email, password, fullName);
   }
 }
---
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -0,0 +1,80 @@
+import 'package:cars_app/features/auth/domain/domain.dart';
+import 'package:cars_app/features/auth/infraestructure/infraestructure.dart';
+import 'package:cars_app/features/users/users.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum AuthStatus {
+  chenking,
+  authenticated,
+  notAuthenticated,
+}
+
+final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
+  final authRepository = AuthRepositoryImpl();
+  return AuthNotifier(
+    authRepository: authRepository,
+  );
+});
+
+class AuthNotifier extends StateNotifier<AuthState> {
+  final AuthRepository authRepository;
+
+  AuthNotifier({
+    required this.authRepository,
+  }) : super(AuthState());
+
+  void loginUser(String email, String password) async {
+    try {
+      final user = await authRepository.login(email, password);
+      _setLoggedUser(user);
+    } on WrongCredential {
+      logout(error: 'Credenciales incorrectas');
+    } catch (e) {
+      logout(error: 'Algo salió mal... Error no controlado');
+    }
+  }
+
+  void registerUser(String email, String password, String fulName) async {}
+  void checkAuthStatus() async {}
+
+  void _setLoggedUser(User user) {
+    // TODO: necesito guardar el token en el dispositivo.
+    state = state.copyWith(
+      user: user,
+      errorMessage: '',
+      authStatus: AuthStatus.authenticated,
+    );
+  }
+
+  Future<void> logout({String? error}) async {
+    // TODO: limpiar el token
+    state = state.copyWith(
+      authStatus: AuthStatus.notAuthenticated,
+      user: null,
+      errorMessage: error,
+    );
+  }
+}
+
+class AuthState {
+  final AuthStatus authStatus;
+  final User? user;
+  final String errorMessage;
+
+  AuthState({
+    this.authStatus = AuthStatus.chenking,
+    this.user,
+    this.errorMessage = '',
+  });
+
+  AuthState copyWith({
+    AuthStatus? authStatus,
+    User? user,
+    String? errorMessage,
+  }) =>
+      AuthState(
+        authStatus: authStatus ?? this.authStatus,
+        user: user ?? this.user,
+        errorMessage: errorMessage ?? this.errorMessage,
+      );
+}
