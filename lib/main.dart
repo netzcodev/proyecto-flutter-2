fe39faee3f119a81b929de5b39a153450b36f284
Brayan Vargas <brayanvargasbarajas@gmail.com>
2023-09-05 14:38:38 -0500

    add env variables and implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:cars_app/config/config.dart';
-import 'package:cars_app/config/router/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  await Environment.initEnvironment();
+
   runApp(
     const ProviderScope(
       child: MainApp(),
